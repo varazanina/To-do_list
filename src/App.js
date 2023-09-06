7e719eb71bc70c0af5bc867e63fbad783ef7d1cc
varazanina <113165723+varazanina@users.noreply.github.com>
2023-09-06 23:23:17 +0200

    kjh

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoApp from './components/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Todo.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+function TodoApp() {
+  /* todos is an array that stores all the tasks */
+  const [todos, setTodos] = useState([]);
+  /* task is the input from the input field */
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    /* trim function to find out whether the input from the user isnt empty */
+    if (task.trim() !== '') {
+      /* spread operator is used so i dont directly manipulate with the array, the new task is added to the original array */
+      setTodos([...todos, { text: task, completed: false }]);
+      /* clearing the input field */
+      setTask('');
+    }
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    /* 1 is for number f objects removed from the (copy of the) array */
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className='todoApp'>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={task}
+          /* `onChange` (event hadler attribute) is called when the input value changes - so while i am writing into the input field 
+          `e` is an object of the arrow function that defines what should happen when the onChange event happens
+          `e.target` refers to the input element that triggered the event
+          `e.target.value` retrieves the current value of the input element*/
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(index)}
+            />
+            {todo.completed ? <del>{todo.text}</del> : todo.text}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoApp;
